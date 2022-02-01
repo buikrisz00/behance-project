cd8389f439a672cfd506384bf058c3394580cd2a
Krisztian Bui <buikrisz00@gmail.com>
2022-02-01 23:43:34 +0100

    Offers page created

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,13 @@
 function changeLeft () {
-    document.getElementById("root").style.backgroundImage = "url('Images/about_img.PNG')"
+    document.getElementById("root").style.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('Images/home_background_2.jpg')"
+    document.getElementById("root").style.backgroundRepeat = "no-repeat"
+    document.getElementById("root").style.backgroundPosition = "center"
+    document.getElementById("root").style.backgroundSize = "cover"
+}
+
+function changeRight () {
+    document.getElementById("root").style.background = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('Images/home_background.jpg')"
+    document.getElementById("root").style.backgroundRepeat = "no-repeat"
+    document.getElementById("root").style.backgroundPosition = "center"
+    document.getElementById("root").style.backgroundSize = "cover"
 }
